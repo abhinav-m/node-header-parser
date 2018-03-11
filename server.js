cc3e953ec28b6596bf6e3ce09a1879a61178f26f
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-11 18:39:13 +0530

    Added changes

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   const os = req.headers["user-agent"];
-  const ip = req.connection.remoteAddress;
+  //https://stackoverflow.com/questions/8107856/how-to-determine-a-users-ip-address-in-node
+  const ip = req.headers["x-forwarded-for"]
+    ? req.headers["x-forwarded-for"].split(",").pop()
+    : undefined ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+
   const lang = req.headers["accept-language"].split(",")[0];
   const stringRes = JSON.stringify({
     os,
